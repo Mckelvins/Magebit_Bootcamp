4b683cbe5ba2901336a3c1368a93219fd155e7b7
Kalvis Krišjānis-Reiniks <65552072+Mckelvins@users.noreply.github.com>
2022-08-09 16:48:44 +0300

    Add files via upload

---
+++ Lesson7_skeleton/loader.js
@@ -0,0 +1,16 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 10);
+}
+
+/*
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+*/
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
