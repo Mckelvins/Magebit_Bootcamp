9d4043f93d3cc7e589e85a0ffcf1aabe428daf76
Kalvis Krišjānis-Reiniks <65552072+Mckelvins@users.noreply.github.com>
2022-08-10 07:51:05 +0300

    some changed was made

--- Lesson7_skeleton/loader.js
+++ Lesson7_skeleton/loader.js
@@ -1,15 +1,10 @@
+
 var myVar;
 
 function myFunction() {
-  myVar = setTimeout(showPage, 10);
+  myVar = setTimeout(showPage, 3000);
 }
 
-/*
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
-*/
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
